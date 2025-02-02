df3bfa36c7badaf3f927063d79e579c254719284
Laguna Studios <flo@lagunastudios.de>
2025-02-02 14:30:23 +0100

    improved in app browser

--- lib/ui/core/screen/webbrowser_screen.dart
+++ lib/ui/core/screen/webbrowser_screen.dart
@@ -3,19 +3,26 @@ import "package:flutter_inappwebview/flutter_inappwebview.dart";
 import "package:vault/context_extension.dart";
 
 class WebbrowserScreen extends StatelessWidget {
+  static Future<T?> open<T>(BuildContext context, {required String title, required String url}) => context.go<T>(WebbrowserScreen(title: title, url: url));
 
-  static Future<T?> open<T>(BuildContext context, String url) => context.go<T>(WebbrowserScreen(url: url));
-
+  final String title;
   final String url;
 
-  const WebbrowserScreen({super.key, required this.url});
+  const WebbrowserScreen({super.key, required this.title, required this.url});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
       body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri("https://lagunastudios.de/github/vault/refs/heads/main/html/privacy.html")),
+        initialOptions: InAppWebViewGroupOptions(
+          android: AndroidInAppWebViewOptions(useOnRenderProcessGone: true),
+        ),
+        initialUrlRequest:
+            URLRequest(url: Uri.parse("https://lagunastudios.de/github/vault/refs/heads/main/html/privacy.html")),
       ),
     );
   }
