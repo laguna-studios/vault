09e9de90530ebbf6dc63fb1e36898d23133adab3
Laguna Studios <flo@lagunastudios.de>
2025-02-18 13:07:46 +0100

    change webview package

--- lib/ui/core/screen/webbrowser_screen.dart
+++ lib/ui/core/screen/webbrowser_screen.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
-import "package:flutter_inappwebview/flutter_inappwebview.dart";
 import "package:vault/context_extension.dart";
+import "package:webview_flutter/webview_flutter.dart";
 
-class WebbrowserScreen extends StatelessWidget {
+class WebbrowserScreen extends StatefulWidget {
   static Future<T?> open<T>(BuildContext context, {required String title, required String url}) => context.go<T>(WebbrowserScreen(title: title, url: url));
 
   final String title;
@@ -11,19 +11,28 @@ class WebbrowserScreen extends StatelessWidget {
   const WebbrowserScreen({super.key, required this.title, required this.url});
 
   @override
+  State<WebbrowserScreen> createState() => _WebbrowserScreenState();
+}
+
+class _WebbrowserScreenState extends State<WebbrowserScreen> {
+
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController();
+    _controller.loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         centerTitle: true,
       ),
-      body: InAppWebView(
-        initialOptions: InAppWebViewGroupOptions(
-          android: AndroidInAppWebViewOptions(useOnRenderProcessGone: true),
-        ),
-        initialUrlRequest:
-            URLRequest(url: Uri.parse("https://lagunastudios.de/github/vault/refs/heads/main/html/privacy.html")),
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
