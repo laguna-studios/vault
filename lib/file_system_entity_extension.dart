fe46a65fdd81e2e9ba93ebb2f971aec1d31be0fc
Laguna Studios <flo@lagunastudios.de>
2025-07-19 20:18:04 +0200

    fix file display problems

--- lib/file_system_entity_extension.dart
+++ lib/file_system_entity_extension.dart
@@ -3,13 +3,13 @@ import "dart:io";
 import "package:dartx/dartx_io.dart";
 
 extension FileSystemEntityExtension on FileSystemEntity {
-  bool get isImage => ["jpg", "jpeg", "png", "gif", "bmp", "webp"].contains(extension);
+  bool get isImage => [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"].contains(extension);
   bool get isVideo => [
-        "mp4",
-        "webm",
-        "3gp",
-        "mov",
-        "m4v",
-        "ogg",
+        ".mp4",
+        ".webm",
+        ".3gp",
+        ".mov",
+        ".m4v",
+        ".ogg",
       ].contains(extension);
 }
